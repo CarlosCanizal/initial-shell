a621ce57281e7862ae13a25af14c5178a61b9920
Carlos Canizal <carlos@codecery.com>
2014-12-12 12:07:38 -0600

    Creates Shell

---
+++ src/client/app/layout/shell.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.layout')
+    .controller('Shell',Shell);
+
+  // Shell.$inject = ['$scope'];
+
+  function Shell($scope){
+    // jshint validthis: true 
+    var vm = this;
+  }
+
+})();
