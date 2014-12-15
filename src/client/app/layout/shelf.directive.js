9113f69721e69c3674b7af14fd1d3a3838a1cd39
Carlos Canizal <carlos@codecery.com>
2014-12-15 13:41:57 -0600

    Creates directive to implement Shelf

---
+++ src/client/app/layout/shelf.directive.js
@@ -0,0 +1,34 @@
+// (function() {
+//   'use strict';
+
+angular
+  .module('app.layout')
+  .directive('shelf',shelf);
+
+shelf.$inject = ['serieApi'];
+
+function shelf(serieApi){
+  return{
+    restrict: 'EA',
+    scope:{
+      publisher : '@publisher'
+    },
+    templateUrl: 'app/layout/shelf.template.html',
+    controller:function($scope){
+      $scope.vm = {};
+      $scope.vm.suscribe =  $scope.$parent.suscribe;
+    },
+    link:function(scope,element,attr){
+      var publisher = attr.publisher;
+      scope.vm.title = publisher;
+      serieApi.getSeries({publisher:publisher}).then(function(series){
+        console.log(series.result);
+        scope.vm.itemsList = series.result;
+      },function(error){
+        console.error('status: '+error.status+', statusText: '+error.statusText+', error: '+error.data.error);
+      });
+    }
+  }
+}
+
+// });
--- src/client/app/layout/shell.js
+++ src/client/app/layout/shell.js
@@ -11,7 +11,8 @@
     // jshint validthis: true 
     var vm = this;
     vm.searchValue = null;
-    vm.results = [];
+    vm.itemsList = [];
+    vm.title = 'Resultados';
 
     vm.searchByName = function(){
       var params = {
@@ -19,10 +20,14 @@
       }
       serieApi.getSeries(params).then(function(series){
         console.log(series.result);
-        vm.results = series.result;
+        vm.itemsList = series.result;
       },function(error){
         console.error('status: '+error.status+', statusText: '+error.statusText+', error: '+error.data.error);
       });
+    }
+
+    $scope.suscribe = function(){
+      alert('suscribe');
     }
   }
 
