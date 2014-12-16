f652a6ad3a481f6c1964952114ba1d795abaeb5d
Carlos Canizal <carlos@codecery.com>
2014-12-16 10:49:53 -0600

    Reorganize views

--- src/client/app/core/config.js
+++ src/client/app/core/config.js
@@ -2,8 +2,24 @@ angular
 .module('app')
 .config(config)
 
-config.$inject = ['$urlRouterProvider','$stateProvider'];
+config.$inject = ['$locationProvider', '$urlRouterProvider','$stateProvider'];
 
-function config($urlRouterProvider, $stateProvider) {
+function config($locationProvider,$urlRouterProvider, $stateProvider) {
+
+  $locationProvider.html5Mode(true);
+
+  $stateProvider
+    .state('home', {
+      url:'/',
+      templateUrl: 'app/layout/shell.html'
+    })
+    .state('suscribe',{
+      url:'/suscripciones',
+      templateUrl:'app/suscribe/suscribe.template.html'
+    })
+    .state('about',{
+      url:'/como-funciona',
+      templateUrl:'app/about/about.template.html'
+    });
   $urlRouterProvider.otherwise('/');
 }
