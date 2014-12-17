4741c07f3c182e6b83c848d86f5e3b7636912712
Carlos Canizal <carlos@codecery.com>
2014-12-17 13:04:44 -0600

    Adds gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,25 @@
 var gulp = require('gulp');
 var paths = require('./gulp/gulp.config.json');
-
+// var sass = require('gulp-sass');
 var $ = require('gulp-load-plugins')();
 
-gulp.task('default', ['watch','jade']);
+gulp.task('default', ['watch','jade','sass']);
 
 gulp.task('watch', function() {
-  // gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.jade, ['jade']);
+  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.jade, ['jade', 'sass']);
 });
 
-// gulp.task('sass', function () {
-//   return gulp.src(paths.sass)
-//     .pipe($.plumber())
-//     .pipe($.rubySass({
-//       style: 'expanded',
-//       sourcemap: true
-//     }))
-//     .pipe($.autoprefixer('last 1 version'))
-//     .pipe(gulp.dest(function(file) {
-//       return file.base; 
-//     }))
-//     .pipe($.size());
-// });
+gulp.task('sass', function () {
+  return gulp.src(paths.sass)
+    .pipe($.plumber())
+    .pipe($.sass({sourceComments:'normal'}))
+    .pipe($.autoprefixer('last 1 version'))
+    .pipe(gulp.dest(function(file) {
+      return file.base; 
+    }))
+    .pipe($.size());
+});
 
 gulp.task('jade', function () {
   return gulp.src(paths.jade)
