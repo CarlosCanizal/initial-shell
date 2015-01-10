282f405bef0fcbd4bbea30500a5bfcd63fc3826b
Carlos Canizal <carlos@codecery.com>
2015-01-09 22:41:20 -0600

    Validates login form

--- src/client/app/login/loginForms.directive.js
+++ src/client/app/login/loginForms.directive.js
@@ -10,13 +10,22 @@ function loginForms(userApi, storage){
     templateUrl: 'app/login/login.forms.html',
     scope: true,
     link:function(scope,element,attr){
+      scope.response = false;
+      
       scope.login = function(){
-        userApi.login(scope.user).then(function(user){
-          scope.setUser(user);
-        },function(error){
-          console.log(error);
-          console.error('status: '+error.status+', statusText: '+error.statusText+', error: '+error.data.error);
-        });
+        scope.response = false;
+        if(scope.loginForm.$valid){
+          userApi.login(scope.user).then(function(user){
+            scope.setUser(user);
+          },function(error){
+            scope.response = error.data.error;
+            console.log(error);
+            console.error('status: '+error.status+', statusText: '+error.statusText+', error: '+error.data.error);
+          });
+        }else{
+          scope.loginForm.username.$setDirty();
+          scope.loginForm.password.$setDirty();
+        }
       }
 
       scope.register = function(){
