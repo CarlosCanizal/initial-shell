5867fef32cbcb8c86cceead4c76143880b236a2d
Carlos Canizal <carlos@codecery.com>
2015-01-26 12:36:10 -0600

    Updates coupon response

--- src/client/app/wallet/coupon/coupon.directive.js
+++ src/client/app/wallet/coupon/coupon.directive.js
@@ -10,14 +10,17 @@ function coupon(couponApi){
     scope: true,
     templateUrl: 'app/wallet/coupon/coupon.form.html',
     link:function(scope, element, attribute){
-      
       scope.redeemCode = function(){
+        scope.error = false;
+        scope.success = false;
         if(scope.couponForm.$valid){
           scope.showLoading();
           couponApi.redeemCode(scope.coupon.code).then(function(code){
             console.log('redeemCode',code);
+            scope.success = "Codigo valido";
           },function(error){
             console.error(error);
+            scope.error = error.data.error;
           }).finally(scope.hideLoading);
         }
       }
