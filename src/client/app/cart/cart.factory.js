6b94a9dc5dca17f1974f27a6d2fe99f9ba1cb9cf
Carlos Canizal <carlos@codecery.com>
2015-01-28 21:30:39 -0600

    Updates cart wallet

--- src/client/app/cart/cart.factory.js
+++ src/client/app/cart/cart.factory.js
@@ -28,7 +28,7 @@
 
       var cart = storage.get('cart');
       if(!cart){
-        cart = {items:[], itemsTotal: 0, cartTotal :0, total:0,useWallet:false, wallet:0, userWallet:0, shippingAddress:{}, paymentMethod:{}}
+        cart = {items:[], itemsTotal: 0, cartTotal :0, total:0,useWallet:false, wallet:0, userWallet:0, descuento:0, shippingAddress:{}, paymentMethod:{}}
         // storage.set('cart',cart)
         this.setCart(cart);
       }
@@ -58,7 +58,7 @@
       var total = cartTotal;
       if(cart.useWallet && (cart.userWallet && cart.userWallet > 0)){
         total = (cartTotal-cart.userWallet) > 0 ? cartTotal-cart.userWallet : 0;
-        cart.wallet = (cart.userWallet - cartTotal > 0 )? cart.userWallet - cartTotal : 0;
+        cart.wallet = (cart.userWallet - cartTotal > 0 )? cartTotal : 0;
       }else{
         total = cartTotal;
         cart.wallet = 0;
