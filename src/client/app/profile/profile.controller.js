804c8e79e7950c550900151c1c756e3761ab7046
Carlos Canizal <carlos@codecery.com>
2015-02-07 23:10:42 -0600

    Creates function to update password

--- src/client/app/profile/profile.controller.js
+++ src/client/app/profile/profile.controller.js
@@ -15,27 +15,35 @@
     profile.newPassword = null;
     profile.oldPassword = null;
 
-    $scope.saveProfile = function(){
-      shell.showLoading();
-      // var sessionToken = profile.user.sessionToken;
-      userApi.saveProfile(profile.user).then(function(user){
-        //siempre que se actualize el user debe ir updateCurrentUser();
-        shell.updateCurrentUser();
-      },function(error){
-        shell.setError(error);
-      }).finally(shell.hideLoading);
+    profile.saveProfile = function(){
+      if($scope.profileForm.$valid){
+        shell.showLoading();
+        // var sessionToken = profile.user.sessionToken;
+        userApi.saveProfile(profile.user).then(function(user){
+          //siempre que se actualize el user debe ir updateCurrentUser();
+          shell.updateCurrentUser();
+        },function(error){
+          shell.setError(error);
+        }).finally(shell.hideLoading);
+      }
     }
 
-    $scope.updatePassword = function(){
-      shell.showLoading();
-      var oldPassword = profile.oldPassword;
-      var newPassword = profile.newPassword;
-      userApi.updatePassword(profile.user,oldPassword, newPassword).then(function(user){
-        shell.setMessage('Contraseña actualizada con exito');
-        shell.updateCurrentUser();
-      },function(error){
-        shell.setError(error);
-      }).finally(shell.hideLoading);
+    profile.updatePassword = function(){
+      if($scope.passwordForm.$valid){
+        shell.showLoading();
+        var oldPassword = profile.oldPassword;
+        var newPassword = profile.newPassword;
+        userApi.updatePassword(profile.user,oldPassword, newPassword).then(function(user){
+          shell.setMessage('Contraseña actualizada con exito');
+          profile.newPassword = null;
+          profile.oldPassword = null;
+          $scope.passwordMatch = null;
+          $scope.passwordForm.$setPristine();
+          shell.updateCurrentUser();
+        },function(error){
+          shell.setError(error);
+        }).finally(shell.hideLoading);
+      }
     }
     
   }
