51971cdfd456fa63387efcab9712f733b975e56f
Carlos Canizal <carlos@codecery.com>
2015-02-09 17:47:23 -0600

    Creates function to get items most sale in time period

--- src/client/app/admin/sales/sales.controller.js
+++ src/client/app/admin/sales/sales.controller.js
@@ -14,28 +14,48 @@
     sales.startDate = today;
     sales.endDate = today;
     sales.result = {};
+    sales.itemsList;
 
     shell.showLoading();
     salesApi.getSales().then(function(result){
-      console.log(result);
-      var result = result.result;
-      sales.result = result;
+      sales.result = result.result;
     },function(){
       shell.setError(error);
     }).finally(shell.hideLoading);
 
-
-
-
     sales.getSales = function(startDate, endDate){
       shell.showLoading();
       salesApi.getSales({startDate:startDate, endDate:endDate}).then(function(result){
-        var result = result.result;
-        sales.result = result;
+        sales.result = result.result;
       },function(error){
         shell.setError(error);
       }).finally(shell.hideLoading)
     }
+
+    $scope.$watch('sales.result.orders',function(){
+      if(sales.result.orders && sales.result.orders.length > 0 ){
+        var orders = sales.result.orders;
+        var byTotal = {};
+        var filtered = [];
+        angular.forEach(orders, function(order){
+          angular.forEach(order.items, function(item){
+            console.log('id',item.objectId);
+            console.log('quantity', item.quantity);
+            if(item.objectId in byTotal){
+              byTotal[item.objectId].total += item.quantity;
+            }else{
+              byTotal[item.objectId] = {item:item, total: item.quantity};
+            }
+
+          });
+        });
+        angular.forEach(byTotal, function(item){
+          filtered.push(item);
+        });
+
+        sales.itemsList = filtered;
+      }
+    });
 
   }
 
